567d9b47813bb2a70668370a0e0c07e697d9a5c7
mzehrer <zehrer@zepan.org>
2013-01-20 15:38:48 +0100

    Update copyright

--- src/main/java/org/zepan/elasticsearch/river/pinboard/PinboardRiver.java
+++ src/main/java/org/zepan/elasticsearch/river/pinboard/PinboardRiver.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013 Michael Zehrer <zehrer@zepan.org>
+ *
+ * This file is part of pinboard-river.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.zepan.elasticsearch.river.pinboard;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,7 +45,6 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-
 /**
  * Created with IntelliJ IDEA.
  * User: michael
@@ -39,6 +56,7 @@ public class PinboardRiver extends AbstractRiverComponent implements River {
 
     private volatile BulkProcessor bulkProcessor;
     private final Client client;
+    private RiverSettings riverSettings;
 
     private final String indexName;
     private final String typeName;
@@ -53,6 +71,7 @@ public class PinboardRiver extends AbstractRiverComponent implements River {
     protected PinboardRiver(RiverName riverName, RiverSettings riverSettings, Client client) {
         super(riverName, riverSettings);
         this.client = client;
+        this.riverSettings = riverSettings;
 
         String user = null;
         String token = null;
@@ -107,6 +126,7 @@ public class PinboardRiver extends AbstractRiverComponent implements River {
     public void start() {
 
         logger.info("Starting Pinboard river");
+        logger.info("Pinboard index will be refreshed every {} seconds", this.fetchInterval);
 
         if (!client.admin().indices().prepareExists(indexName).execute().actionGet().exists()) {
 
